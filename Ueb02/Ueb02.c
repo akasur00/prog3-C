fe6780e1c9a1994118e4b6bbc12f0eda25fa34d8
it.willihuebner@protonmail.com <it.willihuebner@protonmail.com>
2023-10-09 15:37:26 +0200

    overflow.c now works

--- Ueb02/Ueb02.c
+++ Ueb02/Ueb02.c
@@ -22,7 +22,7 @@ int main() {
             overflow();
             break;
         default:
-            return -1;
+            return 0;
     }
     return 0;
 }
--- Ueb02/overflow.c
+++ Ueb02/overflow.c
@@ -2,31 +2,26 @@
 // Created by Akasur on 08.10.2023.
 //
 #include <stdio.h>
+#define INT_MAX (2147483647)
+#define INT_MIN (-2147483648)
 
 int overflow() {
     int zahl1;
     int zahl2;
-    int summe;
-    int overflow;
 
     //Start und Eingabe
     printf("<--Programm zur Erkennung eines Ueberlaufes bei der Addition mit zwei Int-Werten-->\n");
     printf("Geben sie zwei Ganzzahlige Ziffern oder (0) (0) ein um immer einen Ueberlauf zu provozieren\n");
     printf("Erste Zahl:");
     scanf("%d", &zahl1);
-    printf("\nZweite Zahl:");
+    printf("Zweite Zahl:");
     scanf("%d", &zahl2);
 
-
-    //Ausgabe
-    printf("Die Addition ergab: %i", summe, "\n");
-    switch (overflow) {
-        case 1:
-            printf("Es kam zu einem Overflow.");
-            break;
-        case 0:
-            printf("Es gab keinen Overflow.");
-            break;
-    }
+    if(zahl1 > 0 && zahl2 > INT_MAX - zahl1)
+        printf("Es kam zu einem Overflow.");
+    else if(zahl1 < 0 && zahl2 < INT_MIN - zahl1)
+        printf("Es kam zu einem Underflow.");
+    else
+        printf("Ergebnis: %i", zahl1 + zahl2);
     return 0;
 }
