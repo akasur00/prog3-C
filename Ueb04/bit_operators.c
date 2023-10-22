246fbce968303d0a0c1ee4326b1dcd408df8d765
akasur00 <100120958+akasur00@users.noreply.github.com>
2023-10-22 16:00:23 +0200

    bit_operators.c completed

--- Ueb04/bit_operators.c
+++ Ueb04/bit_operators.c
@@ -1,10 +1,64 @@
 //
 // Created by Akasur on 22.10.2023.
 //
-
+#include "stdio.h"
 #include "bit_operators.h"
 
+int printBits_int(int n){
+    //int n2 = n;
+    //int bits [32];
+    //int length = sizeof(bits)/ sizeof(bits[0]);
+
+    //fill array
+    //for (int i = 0; i < length; ++i) {
+    //        if (n2 % 2 == 1 || n2 % 2 == -1)
+    //            bits[length - i - 1] = 1;
+    //        else
+    //            bits[length - i - 1] = 0;
+    //        n2 /= 2;
+    //}
+
+    //printf("%d:\t\t", n);
+    //for (int i = 0; i < length; ++i) {
+    //    if(i % 4 == 0 && i > 0)
+    //        printf(".");
+    //    printf("%d", bits[i]);
+    //}
+    printf("%d:\t", n);
+    int j = 0;
+    //for loop with bitwise variable
+    for (unsigned int i=1<<31; i > 0; i >>= 1) {
+        j++;
+        if(n & i)
+            printf("1");
+        else
+            printf("0");
+        //set point
+        if ((j % 4) == 0 && i>1)
+            printf(".");
+    }
+    return 0;
+}
+
 int bit_operators(){
+    int input, mask;
+    printf("<--program for the display of bits for integers-->\n");
+    printf("please input a number to be displayed:\n");
+    scanf(" %i", &input);
+    printf("input:\t\t"), printBits_int(input);
+    printf("\nl shift <<2:\t"), printBits_int(input<<2);
+    printf("\nr shift >>2:\t"), printBits_int(input>>2);
+    printf("\ncomb shifts:\t"), printBits_int((input>>2)<<2);
+    printf("\nnegation:\t"), printBits_int(~input);
+
+    printf("\n\nnow input a number to be used as a mask:\n");
+    scanf(" %i", &mask);
+    printf("\n<--other Operators-->");
+    printf("\ninput:\t\t"), printBits_int(input);
+    printf("\nmask:\t\t"), printBits_int(mask);
+    printf("\nAND &:\t\t"), printBits_int(input&mask);
+    printf("\nXOR ^:\t\t"), printBits_int(input^mask);
+    printf("\nOR |:\t\t"), printBits_int(input|mask);
 
 
     return 0;
--- Ueb04/xor_crypt.c
+++ Ueb04/xor_crypt.c
@@ -1,10 +1,43 @@
 //
 // Created by Akasur on 22.10.2023.
 //
-
+#include "stdio.h"
 #include "xor_crypt.h"
 
-int xor_crypt(){
+//Create key from array
+char randchar(char input []){
+    char key;
+    int sum;
+    for (int i = 0; i < 256; ++i) {
+        sum +=input[i];
+    }
+    key = sum % 256;
+    return key;
+}
 
+void crypt_8bit(char input[256], char key){
+    //encryption
+    char encrypted_string [256];
+    for (int i = 0; i < 256; ++i) {
+        encrypted_string[i] = input[i]^key;
+    }
+    printf("\n\nencrypted string:\n%s", encrypted_string);
+
+    //decryption
+    char decrypted_string [256];
+    for (int i = 0; i < 256; ++i) {
+        decrypted_string[i] = encrypted_string[i]^key;
+    }
+    printf("\n\ndecrypted string:\n%s", decrypted_string);
+}
+
+int xor_crypt(){
+    char in [256];
+    printf("<--program for encryption and decryption of strings based of XOR-operations-->\n");
+    printf("please input a  string for encryption and a char as the key:\n");
+    scanf("%s", &in);
+    printf("\nInput was: %s", in);
+    printf("\nkey was: %c", randchar(in));
+    crypt_8bit(in, randchar(in));
     return 0;
 }
