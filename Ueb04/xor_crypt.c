0f57f9cde1dfe932bf92810a9a1c2d8dd0db8b52
it.willihuebner@protonmail.com <it.willihuebner@protonmail.com>
2023-10-25 14:31:06 +0200

    optimized xor_encrypt

--- Ueb04/xor_crypt.c
+++ Ueb04/xor_crypt.c
@@ -4,6 +4,12 @@
 #include "stdio.h"
 #include "xor_crypt.h"
 
+void crypt(char array[], char key[], int length) {
+    for (int i = 0; i < length; ++i) {
+        array[i] = array[i] ^ key[i];
+    }
+}
+
 int xor_crypt(){
     printf("<--program for encryption and decryption of strings based of XOR-operations-->\n");
 
@@ -12,13 +18,13 @@ int xor_crypt(){
     printf("please input a  string for encryption:\n");
     scanf(" %255s", in);
 
-    //look how long string is
+    //look how long input-string is
     int length = 0;
     while(in[length] != '\000')
         length++;
     length++;    //add 1 more for escape-sequence
 
-    //create new smaller array
+    //create new smaller array (string)
     char dynarray [length];
     for (int i = 0; i < length; ++i) {
         dynarray[i] = in[i];
@@ -29,27 +35,17 @@ int xor_crypt(){
         for (int i = 0; i < length; ++i) {
             key[i] = dynarray[i] % length;
         }
-        //don't forget the escape
-        //key[length] = '\000';
-
-    //Encryption
-    char encrypted_string [length];
-    for (int i = 0; i < length; ++i) {
-        encrypted_string[i] = dynarray[i]^key[i];
-    }
-
-    //decryption
-    char decrypted_string [length];
-    for (int i = 0; i < length; ++i) {
-        decrypted_string[i] = encrypted_string[i]^key[i];
-    }
-
 
     //Console-Output
-    printf("\nInput was:\t\t%s Size: %d", dynarray, sizeof(dynarray));
-    printf("\nkey is:\t\t\t%s", key);
-    printf("\nencrypted string:\t%s Size: %d", encrypted_string, sizeof(encrypted_string));
-    printf("\ndecrypted string:\t%s Size: %d", decrypted_string, sizeof(decrypted_string));
+    printf("\nInput was:\t\t\t%s", dynarray);
+    //printf("\nkey is:\t\t\t%s", key);
+
+    crypt(dynarray, key, length);     //encryption
+    printf("\nencrypted string:\t%s", dynarray);
+
+    crypt(dynarray, key, length);     //decryption
+    printf("\ndecrypted string:\t%s", dynarray);
+
     return 0;
 }
 
